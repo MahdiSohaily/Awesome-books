0136708bb38420f44f31c590abfb18f5d44af2ca
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-19 21:17:22 +0430

    Add object constructor

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,9 @@ const form = document.querySelector('form');
 const title = document.getElementById('book-title');
 const author = document.getElementById('book-Author');
 const books = [];
+
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
