2ecf28281d54e9c103325517fb4a29ea014ecde1
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-20 19:44:01 +0430

    Add  addBook method to the class to add new items

---
+++ src/js/Books.js
@@ -0,0 +1,20 @@
+export default class Book {
+  constructor(title, author) {
+    this._title = title;
+    this._author = author;
+  }
+
+  get title() {
+    return this._title;
+  }
+  get author() {
+    this._author;
+  }
+
+  deleteBook() {}
+
+  addBook(storedBooks) {
+    storedBooks.push(this);
+    localStorage.setItem('books', JSON.stringify(storedBooks));
+  }
+}
