c37e003ae0ac4625c8f714b7ba52ca0ecaffa5a7
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-26 18:17:23 +0430

    Add function to get current date time

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,13 @@
 import List from './components/List.js';
 import Contact from './components/Contact.js';
 import Form from './components/Form.js';
+import { DateTime } from "./modules/luxion.js";
 
 // new DOM Elements
 const pages = document.querySelectorAll('.list-item');
 const container = document.getElementById('app');
+const now = DateTime.now();
+console.log(now.year)
 defaultDisplay();
 
 pages.forEach((item) => {
