8a855106e4304fc6ab872d6ceb580dd0ea3c7c6c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-26 18:10:40 +0430

    Add default display function to reuse code

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,7 @@ import Form from './components/Form.js';
 // new DOM Elements
 const pages = document.querySelectorAll('.list-item');
 const container = document.getElementById('app');
-const myList = new List();
-container.innerHTML = myList.content;
-myList.active();
-myList.deleteBook();
+defaultDisplay();
 
 pages.forEach((item) => {
   item.addEventListener('click', (e) => {
@@ -17,10 +14,7 @@ pages.forEach((item) => {
     switch (page) {
       case 'list':
         {
-          const myList = new List();
-          container.innerHTML = myList.content;
-          myList.active();
-          myList.deleteBook();
+          defaultDisplay();
         }
         break;
       case 'form':
@@ -38,11 +32,15 @@ pages.forEach((item) => {
         }
         break;
       default: {
-        const defaultPage = new List();
-        container.innerHTML = defaultPage.content;
-        defaultPage.active();
-        defaultPage.deleteBook();
+        defaultDisplay();
       }
     }
   });
 });
+
+function defaultDisplay() {
+  const myList = new List();
+  container.innerHTML = myList.content;
+  myList.active();
+  myList.deleteBook();
+}
