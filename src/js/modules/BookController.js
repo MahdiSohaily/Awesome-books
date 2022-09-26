9c1cd5ba779a16d5d9c7deb4fb929c827a0f1533
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-26 21:08:07 +0430

    Update Book controller module

--- src/js/modules/BookController.js
+++ src/js/modules/BookController.js
@@ -1,7 +1,22 @@
 import Book from './Books.js';
 
 export default class BooktoDom {
-  
+  static append(index, { _title: title, _author: author }) {
+    return `
+          <tr >
+            <th>${Number(index) + 1}</th>
+            <td>${title}</td>
+            <td>${author}</td>
+            <td class="actions">
+              <a class="icon" href="#">
+                <img width="20" height="20" src="./src/icon/edit-icon.svg" alt="edit-icon">
+              </a>
+              <a class="icon delete-btn" href="#" id=${index}>
+                <img width="20" height="20" src="./src/icon/delete-icon.svg" alt="delete-icon">
+              </a>
+            </td>
+          </tr>`;
+  }
 
   static checkStorage(target) {
     const dataStored = localStorage.getItem('books');
